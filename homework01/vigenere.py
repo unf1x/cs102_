8a9cb08f9f5b76ec991d2f4c32cf3a9d08b2d68c
Maxim Borovskiy <113019329+unf1x@users.noreply.github.com>
2022-11-30 20:44:03 +0300

    i did viginere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,12 +10,25 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    keyword *= len(plaintext) // len(keyword) + 1
+    for i, j in enumerate(plaintext):
+        gg = ord(j) + ord(keyword[i])
+        if 65 <=ord(j) <= 90:
+            ciphertext += chr(gg % 26 + 65)
+    if 97 <= ord(j) <= 122:
+        ord_a = ord('a')
+        keyword *= len(plaintext) // len(keyword) + 1
+        for indx, chplain in enumerate(plaintext):
+            ciphertext += chr(((ord(chplain) - ord_a) + (ord(keyword[indx]) - ord_a)) % 26 + ord_a)
     return ciphertext
+
+
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -25,4 +38,11 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+    keyword *= len(ciphertext) // len(keyword) + 1
+    for i, j in enumerate(ciphertext):
+        gg = (ord(j) - ord(keyword[i]))
+        if 65 <=ord(j) <= 90:
+            plaintext += chr(gg % 26 + 65)
+        if 97 <= ord(j) <= 122:
+            plaintext += chr(gg % 26 + 97)
     return plaintext
